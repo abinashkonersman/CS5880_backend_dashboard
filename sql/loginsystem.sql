1f7a55c0064189fcfad3364117297e03b1aeff8a
abinash biswal Konersman <dougabi@yahoo.com>
2020-03-15 13:04:57 -0500

    php

--- sql/loginsystem.sql
+++ sql/loginsystem.sql
@@ -61,9 +61,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `contactno`, `posting_date`) VALUES
-(9, 'Anuj', 'Kumar', 'demouser@gmail.com', 'Test@123', '21222222', '2015-11-11 18:30:00'),
-(11, 'Anuj', 'Kumar', 'phpgurukulofficial@gmail.com', 'Test@123', '1234567890', '2019-07-14 09:36:24');
+
 
 --
 -- Indexes for dumped tables
@@ -96,6 +94,86 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 31, 2018 at 09:25 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loginsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `truefalse`
+--
+
+CREATE TABLE `truefalse` (
+  `questionid` int(11) NOT NULL,
+  `question` text NOT NULL,
+  `answer` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `truefalse`
+--
+
+INSERT INTO `truefalse` (`questionid`, `question`, `answer`) VALUES
+(1, 'Question1', 1),
+(2, 'question2', 0),
+(3, 'question3', 1),
+(4, 'question4', 0),
+(5, 'question5', 0),
+(6, 'question6', 0),
+(7, 'question7', 0),
+(8, 'question8', 1),
+(9, 'question9', 1),
+(10, 'question10', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `truefalse`
+--
+ALTER TABLE `truefalse`
+  ADD PRIMARY KEY (`questionid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `truefalse`
+--
+ALTER TABLE `truefalse`
+  MODIFY `questionid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
